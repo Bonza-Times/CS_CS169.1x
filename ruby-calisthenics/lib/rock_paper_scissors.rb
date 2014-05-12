369fa12eddbb9fe577fb60420894da45db216f98
Bonza-Times <tylers.pile@gmail.com>
2014-05-11 23:54:55 -0400

    needs testing

--- ruby-calisthenics/lib/rock_paper_scissors.rb
+++ ruby-calisthenics/lib/rock_paper_scissors.rb
@@ -4,7 +4,45 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    player_1, choice_1 = player1
+    player_2, choice_2 = player2
+
+    case choice_1
+    when "R" # Player 1 chooses rock
+      if choice_2 == "S"
+        return player1 #wins
+      elsif choice_2 == "R"
+        return player1 #tie goes to player1
+      elsif choice_2 == "P"
+        return player2 #loses
+      else
+        raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+      end
+    when "P" # Player 1 chooses paper
+      if choice_2 == "S"
+        return player2 #loses
+      elsif choice_2 == "R"
+        return player1 #wins
+      elsif choice_2 == "P"
+        return player1 #tie goes to player1
+      else
+        raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+      end
+    when "S" # Player 1 chooses scissors
+      if choice_2 == "S"
+        return player1 #tie goes to player1
+      elsif choice_2 == "R"
+        return player2 #loses
+      elsif choice_2 == "P"
+        return player1 #wins
+      else
+        raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+      end
+    else
+      raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+    end
+    
+    
   end
 
   def self.tournament_winner(tournament)
