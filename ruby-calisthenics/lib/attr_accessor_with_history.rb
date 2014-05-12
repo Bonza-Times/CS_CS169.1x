c3c50b80d5e19814125bc86532581f0316b68b19
Bonza-Times <tylers.pile@gmail.com>
2014-05-12 09:07:35 -0400

    metaprogramming?

--- ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -4,7 +4,23 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{ attr_name + "_history"}
+        @#{ attr_name } = attr_name
+
+        unless @#{attr_name + "_history"}
+          @#{attr_name + "_history"} = []
+          @#{attr_name + "_history"} << nil
+        end
+
+        @#{ attr_name + "_history"} << attr_name
+
+      end
     }
   end
+end
+
+
+class Foo 
+  extend Class
+  attr_accessor_with_history :bar
 end
