22296af76bf604b8fe44a0300f5118e2aa8bfb83
Bonza-Times <tylers.pile@gmail.com>
2014-05-12 09:24:01 -0400

    again

--- ruby-calisthenics/lib/attr_accessor_with_history.rb
+++ ruby-calisthenics/lib/attr_accessor_with_history.rb
@@ -4,16 +4,12 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      def #{ attr_name + "_history"}
-        @#{ attr_name } = attr_name
-
-        unless @#{attr_name + "_history"}
-          @#{attr_name + "_history"} = []
-          @#{attr_name + "_history"} << nil
+      def #{ attr_name }=(value)
+        if !defined? @#{attr_name}_history
+          @#{attr_name}_history = [@#{attr_name}]
         end
-
-        @#{ attr_name + "_history"} << attr_name
-
+        @#{ attr_name } = value
+        @#{ attr_name }_history << value
       end
     }
   end
