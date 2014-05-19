876aa2d077ebbe0c0a7edaabe7cc3296e0c126e5
Bonza-Times <tylers.pile@gmail.com>
2014-05-19 00:45:07 -0400

    setting up vagrant box

--- Vagrantfile
+++ Vagrantfile
@@ -4,24 +4,37 @@
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
 
+MEMORY = 1024 # This matches the digital ocean droplet size
+CPU_COUNT = 1
+
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # All Vagrant configuration is done here. The most common configuration
   # options are documented and commented below. For a complete reference,
   # please see the online documentation at vagrantup.com.
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "hashicorp/precise64"
+  config.vm.box = "coralnexus/ubuntu-12.04-server-amd64"
 
-  # config.vm.provision :shell, :path => "bootstrap.sh"
+  # private network
   config.vm.network "private_network", ip: "192.168.56.105"
-  
+
+  # forwarded port
+  config.vm.network "forwarded_port", guest: 80, host: 3000
+
   # Use NFS for the shared folder
-  config.vm.synced_folder ".", "/vagrant",
+  config.vm.synced_folder ".", "/working",
   id: "core",
   :nfs => true,
+  :nfs_export => true,
   :mount_options => ['nolock,vers=3,udp,noatime']
  
   # If using VirtualBox
   config.vm.provider :virtualbox do |vb|
+    vb.customize ["modifyvm", :id, "--memory", MEMORY.to_s]
+    vb.customize ["modifyvm", :id, "--cpus", CPU_COUNT.to_s]
   end
+
+  # Provision the virtualbox
+  config.vm.provision "shell",
+  path: File.absolute_path("")+"/vm-setup/configure-image-0.10.2.sh";
 end
---
+++ vm-setup/configure-image-0.10.2.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Note: you will be prompted to enter the password several times through the script
+# run with . configure-image-0.10.2.sh
+
+# This script is designed for Ubuntu 11.10
+
+cd ~/
+sudo apt-get update
+sudo apt-get install -y dkms     # For installing VirtualBox guest additions
+
+# Install RVM and ruby 1.9.3 note: may take a while to compile ruby
+sudo apt-get install -y curl
+\curl -L https://get.rvm.io | bash -s stable --ruby=1.9.3
+source /home/ubuntu/.rvm/scripts/rvm
+
+
+# Install sqlite3 dev
+sudo apt-get -y install sqlite3 libsqlite3-dev
+
+# Install nodejs
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
+
+# Install jslint
+cd ~/
+curl -LO http://www.javascriptlint.com/download/jsl-0.3.0-src.tar.gz
+tar -zxvf jsl-0.3.0-src.tar.gz
+cd jsl-0.3.0/src/
+make -f Makefile.ref
+cd ~/
+sudo cp jsl-0.3.0/src/Linux_All_DBG.OBJ/jsl /usr/local/bin
+sudo rm jsl-0.3.0-src.tar.gz
+sudo rm -rf ~/jsl-0.3.0
+
+# Install other programs
+sudo apt-get install -y git
+sudo apt-get install -y chromium-browser
+sudo apt-get install -y graphviz
+sudo apt-get install -y libpq-dev
+sudo apt-get install nfs-common portmap # nfs shared folder with vagrant
+
+
+## Editors
+# just use nano
+
+## GEMS
+
+# install rails 3.2.13
+gem install rails -v 3.2.13
+
+# sqlite 3 gem
+gem install sqlite3-ruby
+
+# other gems
+gem install cucumber -v 1.3.2
+gem install cucumber-rails -v 1.0.0
+gem install cucumber-rails-training-wheels
+gem install rspec
+gem install autotest
+gem install spork
+gem install metric_fu
+gem install debugger
+gem install timecop -v 0.6.1
+gem install chronic -v 0.9.1
+gem install omniauth
+gem install omniauth-twitter
+gem install nokogiri
+gem install ruby-tmdb -v 0.2.1
+gem install ruby-graphviz
+gem install reek
+gem install flog
+gem install flay
+rvm 1.9.3 do gem install jquery-rails
+gem install fakeweb
