93ae848df875f60ba4fcb7ceedc07f15c78c36fe
vagrant <vagrant@ubuntu-12-04-server.(none)>
2014-05-26 01:20:08 +0000

    first attempt, still weirded out by BDD

--- bdd-cucumber/rottenpotatoes/features/step_definitions/movie_steps.rb
+++ bdd-cucumber/rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,11 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!({:title => movie["title"],
+                    :rating => movie["rating"],
+                    :release_date => movie["release_date"]})
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +17,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  body = page.html
+  assert body.index(e1) < body.index(e2)
+  #flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +30,25 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+  Movie.find(:all).each do |movie|
+    title = movie["title"]
+    if page.respond_to? :should
+      page.should have_content(title)
+    else
+      assert page.has_content?(title)
+    end
+  end
+  #flunk "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  flunk "Unimplemented"
+  rating_list.split(',').each do |rating|
+    if uncheck
+      uncheck("ratings_#{rating}")
+    else
+      check("rating_#{rating}")
+    end
+  end
+  #flunk "Unimplemented"
 end
